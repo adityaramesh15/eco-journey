593c145ecd78a13e83cbcd6cdcfcde626ee71e16
mkiry2 <mkiry2@illinois.edu>
2025-11-03 00:02:26 -0600

    Completed prototype version of home page

---
+++ frontend/script.js
@@ -0,0 +1,8 @@
+document.getElementById("create_button").addEventListener("click", () => 
+{window.location.href="create.html";} );
+
+document.getElementById("load_button").addEventListener("click", () => 
+{window.location.href="load.html";} );
+
+document.getElementById("logout_button").addEventListener("click", () => 
+{window.location.href="login.html";} );
