52c2d1cc72b9ac4fcc4d413c24ed4843b1c48b9c
mkiry2 <mkiry2@illinois.edu>
2025-11-04 22:35:57 -0600

    Create page elements and trip form completed

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,24 @@
-document.getElementById("create_button").addEventListener("click", () => 
-{window.location.href="create.html";} );
+const create_button_wrap = document.getElementById("create_button");
+if (create_button_wrap) {
+    create_button_wrap.addEventListener("click", () => 
+    {window.location.href="create.html";} );
+}
 
-document.getElementById("load_button").addEventListener("click", () => 
-{window.location.href="load.html";} );
+const load_button_wrap = document.getElementById("load_button");
+if (load_button_wrap) {
+    load_button_wrap.addEventListener("click", () => 
+    {window.location.href="load.html";} );
+}
 
-document.getElementById("logout_button").addEventListener("click", () => 
-{window.location.href="login.html";} );
+const logout_button_wrap = document.getElementById("logout_button");
+if (logout_button_wrap){
+    logout_button_wrap.addEventListener("click", () => 
+    {window.location.href="login.html";} );
+}
+
+const home_button_wrap = document.getElementById("home_button");
+if (home_button_wrap) {
+    home_button_wrap.addEventListener("click", () => {
+    window.location.href = "home.html";
+  });
+}
